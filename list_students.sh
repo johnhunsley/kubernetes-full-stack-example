7face0d9eebc45d4719f27a11e19515d9fbcaf68
johnhunsley <jphunsley@gmail.com>
2023-11-30 11:32:11 +0000

    curl cmds

--- list_students.sh
+++ list_students.sh
@@ -1,1 +1,2 @@
+curl -v -X POST http://10.104.82.46/api/students -H 'Content-Type:application/json' -d '{"firstName":"john","lastName":"Hunsley"}'
 curl -v -X GET http://10.109.194.223:8080/students
